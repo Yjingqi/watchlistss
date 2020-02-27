222ab6a40e2c8f2fe6e10dabe0473c3955d00d67
Yjingqi <1598655967@qq.com>
2020-02-28 05:40:19 +0800

    首页问题1

--- blog/commands.py
+++ blog/commands.py
@@ -3,33 +3,6 @@ import click
 from blog import db,app
 
 @app.cli.command()
-@click.option('--username',prompt=True,help="用来登录的用户名")
-@click.option('--password',prompt=True,hide_input=True,confirmation_prompt=True,help="用来登录的密码")
-def admin(username,password):
-    db.create_all()
-    user = User.query.first()
-    if user is not None:
-        click.echo('更新用户')
-        user.username = username
-        user.set_password(password)
-    else:
-        click.echo('创建用户')
-        user = User(username=username,name="雷洛")
-        user.set_password(password)
-        db.session.add(user)
-    
-    db.session.commit()
-    click.echo('创建管理员账号完成')
-
-@app.cli.command()
-@click.option('--drop',is_flag=True,help='删除之后再创建')
-def initdb(drop):
-    if drop:
-        db.drop_all()
-    db.create_all()
-    click.echo('初始化数据库')
-
-@app.cli.command()
 def forge():
     db.create_all()
     name = "Bruce"
@@ -52,3 +25,35 @@ def forge():
         db.session.add(movie)
     db.session.commit()
     click.echo('数据导入完成')
+
+@app.cli.command()
+@click.option('--drop',is_flag=True,help='删除之后再创建')
+def initdb(drop):
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('初始化数据库')
+
+
+
+@app.cli.command()
+@click.option('--username',prompt=True,help="用来登录的用户名")
+@click.option('--password',prompt=True,hide_input=True,confirmation_prompt=True,help="用来登录的密码")
+def admin(username,password):
+    db.create_all()
+    user = User.query.first()
+    if user is not None:
+        click.echo('更新用户')
+        user.username = username
+        user.set_password(password)
+    else:
+        click.echo('创建用户')
+        user = User(username=username,name="雷洛")
+        user.set_password(password)
+        db.session.add(user)
+    
+    db.session.commit()
+    click.echo('创建管理员账号完成')
+
+
+
